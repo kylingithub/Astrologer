e7dab7418c7fd345e241296e7c15eb4280ac3162
Chi-Lin Chen <kylingithubdev@gmail.com>
2020-05-25 05:42:58 +0800

    Update test_Astrologer.py

--- test_Astrologer.py
+++ test_Astrologer.py
@@ -18,7 +18,7 @@ class MyTestCase(unittest.TestCase):
             5:1
         })
     def test_life_number(self):
-        self.assertEqual(1,1)
+        self.assertEqual(4,Astrologer.get_life_number(1995,12,13))
         self.assertEqual(9,Astrologer.get_life_number(1993,8,15))
         self.assertEqual(1,Astrologer.get_life_number(1993,8,16))
         self.assertEqual(6,Astrologer.get_life_number(1993,8,30))
@@ -26,6 +26,8 @@ class MyTestCase(unittest.TestCase):
     def test_constellation(self):
         self.assertEqual("獅子座",Astrologer.get_constellation(8,16))
         self.assertEqual("處女座",Astrologer.get_constellation(8,30))
+        self.assertEqual("天蠍座",Astrologer.get_constellation(10,23))
+        self.assertEqual("雙魚座",Astrologer.get_constellation(2,29))
 
 
 if __name__ == '__main__':
