2f53d0754896234c41dae696fdad78603c4e1843
Chi-Lin Chen <kylingithubdev@gmail.com>
2020-06-12 14:31:30 +0800

    Update test_Astrologer.py

--- test_Astrologer.py
+++ test_Astrologer.py
@@ -3,39 +3,35 @@ import Astrologer
 
 class MyTestCase(unittest.TestCase):
 
-    def test_birthday_dict(self):
-        self.assertDictEqual(Astrologer.get_birthday_dict(1995,12,13),{
-            1:3,
-            2:1,
-            3:1,
-            5:1,
-            9:2
-        })
-        self.assertDictEqual(Astrologer.get_birthday_dict(2005,1,13),{
-            1:2,
-            2:1,
-            3:1,
-            5:1
-        })
+    def test_get_each_number(self):
+
+        self.assertListEqual(
+            [1], get_each_number(1)
+        )
+        self.assertListEqual(
+            [1, 2], get_each_number(12)
+        )
+        self.assertListEqual(
+            [2, 1], get_each_number(21)
+        )
+        self.assertListEqual(
+            [1, 4, 5], get_each_number(145)
+        )
     def test_life_number(self):
-        self.assertEqual(4,Astrologer.get_life_number(1995,12,13))
-        self.assertEqual(9,Astrologer.get_life_number(1993,8,15))
-        self.assertEqual(1,Astrologer.get_life_number(1993,8,16))
-        self.assertEqual(6,Astrologer.get_life_number(1993,8,30))
-    
+        self.assertEqual(4, get_life_number(1995, 12, 13))
+        self.assertEqual(3, get_life_number(1, 1, 1))
+        self.assertEqual(5, get_life_number(101, 10, 11))
+        self.assertEqual(8, get_life_number(2000, 12, 30))
+
+
     def test_constellation(self):
-        self.assertEqual("魔羯座",Astrologer.get_constellation(1,1))
-        self.assertEqual("獅子座",Astrologer.get_constellation(8,16))
-        self.assertEqual("處女座",Astrologer.get_constellation(8,30))
-        self.assertEqual("天蠍座",Astrologer.get_constellation(11,1))
-        self.assertEqual("牡羊座",Astrologer.get_constellation(3,21))
-        self.assertEqual("金牛座",Astrologer.get_constellation(4,30))
-        self.assertEqual("巨蟹座",Astrologer.get_constellation(7,9))
-        self.assertEqual("天秤座",Astrologer.get_constellation(9,28))
-        self.assertEqual("射手座",Astrologer.get_constellation(11,23))
-        self.assertEqual("魔羯座",Astrologer.get_constellation(12,24))
-        self.assertEqual("水瓶座",Astrologer.get_constellation(2,5))
-        self.assertEqual("雙魚座",Astrologer.get_constellation(2,29))
+
+        self.assertEqual("獅子座", get_constellation(8, 16))
+        self.assertEqual("處女座", get_constellation(8, 30))
+        self.assertEqual("天蠍座", get_constellation(11, 1))
+        self.assertEqual("天秤座", get_constellation(9, 28))
+        self.assertEqual("水瓶座", get_constellation(2, 5))
+        self.assertEqual("雙魚座", get_constellation(2, 29))
 
 
 if __name__ == '__main__':
